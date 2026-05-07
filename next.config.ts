88ee862fca9d1b3a47dd8f13078fbe5bcf6ce1bf
lony <dlrkgml1108@gmail.com>
2026-05-07 14:27:23 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* 여기에 설정 추가 */
+  output: 'export',
+  basePath: '/Trpg_CW_Template',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
